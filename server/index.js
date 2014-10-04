efcaf653be026f47120f472edab64560be4d9fba
Alexander Schmidt <alexanderschmidt1@gmail.com>
2014-10-04 16:43:02 +0200

    Add basic config file handling methods.

--- server/index.js
+++ server/index.js
@@ -5,4 +5,3 @@ var app = require('./server'),
 
 app.listen(port);
 console.log('Server started on port ' + port);
-
--- server/server.js
+++ server/server.js
@@ -3,9 +3,15 @@
 var path = require('path'),
     express = require('express'),
     app = express(),
-    morgan = require('morgan');
+    morgan = require('morgan'),
+    environment = process.env.NODE_ENV,
+    getConfig = require('./config/config').getConfig,
+    relativeConfigPath = './',
+    config;
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, '../client/build')));
+
+config = getConfig(environment, relativeConfigPath, require);
 
 module.exports = app;
