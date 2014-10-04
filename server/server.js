410d613227c09601ffe7f3fc22774c3bed24e679
Alexander Schmidt <alexanderschmidt1@gmail.com>
2014-10-04 21:56:03 +0200

    Throw error if no environment is set.

--- server/server.js
+++ server/server.js
@@ -4,13 +4,19 @@ var path = require('path'),
     express = require('express'),
     app = express(),
     morgan = require('morgan'),
-    environment = process.env.NODE_ENV || 'development',
+    environment = process.env.NODE_ENV,
     getConfig = require('./config/config').getConfig,
     configPath = path.join(__dirname, 'config'),
     config;
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, '../client/build')));
+
+if (!environment) {
+    throw new Error('Node environment not set (NODE_ENV).');
+} else {
+    console.log('Running with environment:', environment);
+}
 
 config = getConfig(environment, configPath, require);
 
