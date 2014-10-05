de888e30e07540a5c1570b914ba56c64309eb60b
Alexander Schmidt <alexanderschmidt1@gmail.com>
2014-10-05 10:48:32 +0200

    Move mongo initialisation out of the express initialisation.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,29 @@
 'use strict';
 
-var app = require('./server'),
-    port = 3000;
+var path = require('path'),
+    app = require('./server'),
+    port = 3000,
+    environment = process.env.NODE_ENV,
+    getConfig = require('./config/config').getConfig,
+    configPath = path.join(__dirname, 'config'),
+    mongo = require('./db/mongo'),
+    config;
 
-app.listen(port);
-console.log('Server started on port ' + port);
+if (!environment) {
+    throw new Error('Node environment not set (NODE_ENV).');
+} else {
+    console.log('Running with environment:', environment);
+}
+
+config = getConfig(environment, configPath, require);
+
+mongo.connect(config.db)
+    .then(function () {
+        app.listen(port);
+        console.log('Server started on port ' + port);
+    })
+    .catch(function (error) {
+        if (error) {
+            throw new Error('Error starting up: ' + error.message);
+        }
+    });
--- server/server.js
+++ server/server.js
@@ -3,29 +3,9 @@
 var path = require('path'),
     express = require('express'),
     app = express(),
-    morgan = require('morgan'),
-    environment = process.env.NODE_ENV,
-    getConfig = require('./config/config').getConfig,
-    configPath = path.join(__dirname, 'config'),
-    mongo = require('./db/mongo'),
-    config;
+    morgan = require('morgan');
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, '../client/build')));
-
-if (!environment) {
-    throw new Error('Node environment not set (NODE_ENV).');
-} else {
-    console.log('Running with environment:', environment);
-}
-
-config = getConfig(environment, configPath, require);
-
-mongo.connect(config.db)
-    .catch(function (error) {
-        if (error) {
-            throw new Error('Error connecting to the database: ' + error.message);
-        }
-    });
 
 module.exports = app;
