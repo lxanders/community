da9cca535bd0528ff912dbdf23dc4ec24bc0f68c
Alexander Schmidt <alexanderschmidt1@gmail.com>
2014-10-07 12:25:07 +0200

    Don't assign a variable that is never used.

--- server/server.js
+++ server/server.js
@@ -4,13 +4,12 @@ var path = require('path'),
     bodyParser = require('body-parser'),
     express = require('express'),
     app = express(),
-    morgan = require('morgan'),
-    routes;
+    morgan = require('morgan');
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, '../client/build')));
 app.use(bodyParser.json());
 
-routes = require('./routes')(app);
+require('./routes')(app);
 
 module.exports = app;
