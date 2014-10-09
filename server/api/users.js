e21d0a56b405d1bda5b1d7c3e5ab224811d29f97
Alexander Schmidt <alexanderschmidt1@gmail.com>
2014-10-09 20:53:46 +0200

    Fix formatting.

--- server/api/users.js
+++ server/api/users.js
@@ -89,9 +89,7 @@ function checkUsernameAvailability(username, usersCollection) {
             if (count > 0) {
                 error = new Error('Validation error(s).');
                 error.name = 'VALIDATION_ERROR';
-                error.validationErrors = [
-                    { message: 'Username is already taken.' }
-                ];
+                error.validationErrors = [ { message: 'Username is already taken.' } ];
 
                 throw error;
             }
