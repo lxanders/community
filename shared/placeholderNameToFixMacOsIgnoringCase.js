b8a9d54adc01d48339598c46e02857682708d948
Alexander Schmidt <alexanderschmidt1@gmail.com>
2015-03-17 20:21:12 +0100

    Rename IsomorphicApp in preparation for changing the case of the first character
    This is a problem caused by MacOS being case insensitive

---
+++ shared/placeholderNameToFixMacOsIgnoringCase.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var React = require('react'),
+    routesConfig = require('../config/routes'),
+    Fluxible = require('fluxible'),
+    CommunityApp = require('./components/CommunityApp.jsx'),
+    routrPlugin = require('fluxible-plugin-routr');
+
+function createIsomorphicApp() {
+    var isomorphicApp = new Fluxible({
+        component: React.createFactory(CommunityApp)
+    });
+
+    isomorphicApp.plug(routrPlugin({ routes: routesConfig }));
+
+    return isomorphicApp;
+}
+
+module.exports = {
+    createIsomorphicApp: createIsomorphicApp
+};
