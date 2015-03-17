a6e4da27529f9862df5c3a4b8d5137b1ca02b2dd
Alexander Schmidt <alexanderschmidt1@gmail.com>
2015-03-17 20:31:47 +0100

    Add Layout (base) component

--- shared/components/Layout.jsx
+++ shared/components/Layout.jsx
@@ -5,19 +5,22 @@ var React = require('react'),
 
 Layout = React.createClass({
     render: function() {
+        this.props.markup = 'Yeah!';
+
         return (
-            <!DOCTYPE html>
             <html>
                 <head>
                     <meta charSet="utf-8" />
                     <title>Community - come together</title>
                 </head>
                 <body>
+                    <div id="communityApp" dangerouslySetInnerHTML={{ __html: this.props.content }}></div>
                 </body>
+                <script src="public/js/bundle.js"></script>
+                <script dangerouslySetInnerHTML={{ __html: this.props.startScript }}></script>
             </html>
         );
     }
 });
-
 
 module.exports = Layout;
