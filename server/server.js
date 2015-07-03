877e0b6673d2db8960fc5a6a1987fc7a359260fe
Alexander Schmidt <alexanderschmidt1@gmail.com>
2015-07-03 22:38:07 +0200

    Inline too complicated architecture
    This is part of a simplification refactoring

--- server/server.js
+++ server/server.js
@@ -6,26 +6,13 @@ var path = require('path'),
     express = require('express'),
     server = express(),
     morgan = require('morgan'),
-    renderToHtmlWithDoctype = require('./lib/render').renderToHtmlWithDoctype,
     errorHandler = require('./middleware/errorHandler'),
     IsomorphicApp = require('../shared/IsomorphicApp'),
-    isomorphicApp;
-
-function applicationRouteHandler(req, res, next) {
-    var context = isomorphicApp.createContext(),
-        html;
-
-    context.executeAction(navigateAction, { url: req.url }, function (error) {
-        if (error) {
-            return next(error);
-        }
-
-        html = renderToHtmlWithDoctype(isomorphicApp, res);
-
-        res.write(html);
-        res.end();
-    });
-}
+    isomorphicApp,
+    React = require('react'),
+    serialize = require('serialize-javascript'),
+    Layout = require('../shared/components/Layout.jsx'),
+    layoutComponent = React.createFactory(Layout);
 
 isomorphicApp = IsomorphicApp.createIsomorphicApp();
 
@@ -35,6 +22,28 @@ server.use('/public/css', express.static(path.join(__dirname, '../build/css')));
 server.use(bodyParser.json());
 server.use(errorHandler);
 
-server.get('*', applicationRouteHandler);
+server.use(function (req, res, next) {
+    var context = isomorphicApp.createContext(),
+        html;
+
+    context.executeAction(navigateAction, { url: req.url }, function (err) {
+        if (err) {
+            if (err.statusCode && err.statusCode === 404) {
+                next();
+            } else {
+                next(err);
+            }
+            return;
+        }
+
+        html = React.renderToStaticMarkup(layoutComponent({
+            serializedState: serialize(isomorphicApp.dehydrate(context)),
+            content: React.renderToString(context.createElement()),
+            context: context.getComponentContext()
+        }));
+
+        res.send('<!DOCTYPE html>' + html);
+    });
+});
 
 module.exports = server;
