bd0ef730836b2310c11b37757720f0f8a947c184
Alexander Schmidt <alexanderschmidt1@gmail.com>
2015-07-04 09:59:58 +0200

    Add document titles to the demo components

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,15 +1,19 @@
 'use strict';
 
 var React = require('react'),
+    DocumentTitle = require('react-document-title'),
     Jumbotron = require('react-bootstrap/lib/Jumbotron');
 
 var Home = React.createClass({
     render: function () {
         return (
-            <Jumbotron>
-                <h1>Community</h1>
-                <p>Community-Content-Placeholder: Home</p>
-            </Jumbotron>
+            <DocumentTitle title='Community - Home'>
+                <Jumbotron>
+                    <h1>Community</h1>
+
+                    <p>Community-Content-Placeholder: Home</p>
+                </Jumbotron>
+            </DocumentTitle>
         );
     }
 });
